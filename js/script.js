85edcca7797fece124c3cc581a421d7b244ae30c
Hristo Hristov <archri@openmailbox.org>
2017-01-15 18:27:12 +0200

    created an object with multi-word property

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var a = new Object();
+a["b property"] = "b prop";
+// a.b property = "b prop"; - INVALID
+
+console.log(a);
+console.log(a["b property"]);
+
+// console.log(a.b property); - INVALID
