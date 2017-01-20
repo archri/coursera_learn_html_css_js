f81938f6d8ec117dc23494697275288553fd3883
Hristo Hristov <archri@openmailbox.org>
2017-01-20 23:22:01 +0200

    array with undefined elements

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-var a = {
-  prop: "inside",
-  logThis: function() {
-    console.log(this);
-    function logThisNested() {
-      console.log(this);
-    }
-    logThisNested();
-  }
-};
-a.logThis();
+var a = [
+  "hristo",
+  "svetla"
+];
+a[30] = "nqkoi";
+
+for(var i = 0; i < a.length; i++) {
+  console.log(a[i]);
+}
