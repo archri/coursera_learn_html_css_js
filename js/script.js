83d90df58c63829411435d72aba3b7696df8e96b
Hristo Hristov <archri@openmailbox.org>
2017-01-20 23:01:12 +0200

    nested functions this

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-function Hristo() {
-  this.hair = "dark";
-  this.eyes = "green";
-}
-
-Hristo.prototype.smile =
-  function() {
-    console.log(":)");
-  };
-
-var hristo = new Hristo();
-console.log(hristo.eyes);
-hristo.smile();
-console.log(hristo);
+var a = {
+  prop: "inside",
+  logThis: function() {
+    console.log(this);
+    function logThisNested() {
+      console.log(this);
+    }
+    logThisNested();
+  }
+};
+a.logThis();
